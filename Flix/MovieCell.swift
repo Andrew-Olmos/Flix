c6d1a186fd3b52ad043cc2dcd3764a87ecfa04d4
Andrew-Olmos <82090004+Andrew-Olmos@users.noreply.github.com>
2022-09-12 19:46:05 -0700

    Assignment 1 Upload

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  MovieCell
+//
+//  Created by Andrew Olmos on 9/12/22.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
